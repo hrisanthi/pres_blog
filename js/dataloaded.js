9c2e18d2c84e7549fd37b39a8e848b5f505a3b9a
hrisanthi <hrisanthi.kroi@gmail.com>
2015-06-02 12:33:11 -0400

    js done

--- js/dataloaded.js
+++ js/dataloaded.js
@@ -27,30 +27,80 @@ function loadData(xml){
     console.log("Success, loadData() called")
 
 
-    $(xml).find('candidate').each(function(){
+    $(xml).find('democrate').each(function(){
                //console.log("once for every person");
-               var $candidate = $(this);
-               var name = $candidate.attr("name");
-               candidates.push($candidate.attr("name"));
-               var bio = $candidate.find('bio').text();
-               bioArray.push($candidate.find('bio').text());
-               var image = $candidate.find('image').text();
+               var $democrate = $(this);
+               var name = $democrate.attr("name");
+               candidates.push($democrate.attr("name"));
+               var bio = $democrate.find('bio').text();
+               bioArray.push($democrate.find('bio').text());
+               var image = $democrate.find('image').text();
                console.log(name);
 
-
-
-
-
                var candidateHTML ='';
-               candidateHTML += '<h1>' + name + '</h1>';
                candidateHTML += '<img class="image" alt="" src="' + image + '" />';
-               candidateHTML += '<p>' + bio + '</p>';
+               candidateHTML += '<h1 class="name">' + name + '</h1>';
+               candidateHTML += '<p class="bio">' + bio + '</p>';
 
-
-
-               $(".candidate").append($(candidateHTML));
+               $(".democrate").append($(candidateHTML));
 
              });
+
+    $(xml).find('expectedDems').each(function(){
+                var $expectedDems = $(this);
+                var name = $expectedDems.attr("name");
+                candidates.push($expectedDems.attr("name"));
+                var bio = $expectedDems.find('bio').text();
+                bioArray.push($expectedDems.find('bio').text());
+                var image = $expectedDems.find('image').text();
+                console.log(name);
+
+                var candidateHTML ='';
+                candidateHTML += '<img class="image" alt="" src="' + image + '" />';
+                candidateHTML += '<h1 class="name">' + name + '</h1>';
+                candidateHTML += '<p class="bio">' + bio + '</p>';
+
+                $(".expectedDems").append($(candidateHTML));
+
+            });
+
+            $(xml).find('republican').each(function(){
+                        var $republican = $(this);
+                        var name = $republican.attr("name");
+                        candidates.push($republican.attr("name"));
+                        var bio = $republican.find('bio').text();
+                        bioArray.push($republican.find('bio').text());
+                        var image = $republican.find('image').text();
+                        console.log(name);
+
+                        var candidateHTML ='';
+                        candidateHTML += '<img class="image" alt="" src="' + image + '" />';
+                        candidateHTML += '<h1 class="name">' + name + '</h1>';
+                        candidateHTML += '<p class="bio">' + bio + '</p>';
+
+                        $(".republican").append($(candidateHTML));
+
+                    });
+
+                    $(xml).find('expectedReps').each(function(){
+                                var $expectedReps = $(this);
+                                var name = $expectedReps.attr("name");
+                                candidates.push($expectedReps.attr("name"));
+                                var bio = $expectedReps.find('bio').text();
+                                bioArray.push($expectedReps.find('bio').text());
+                                var image = $expectedReps.find('image').text();
+                                console.log(name);
+
+                                var candidateHTML ='';
+                                candidateHTML += '<img class="image" alt="" src="' + image + '" />';
+                                candidateHTML += '<h1 class="name">' + name + '</h1>';
+                                candidateHTML += '<p class="bio">' + bio + '</p>';
+
+                                $(".expectedReps").append($(candidateHTML));
+
+                            });
+
+
 
 
 
